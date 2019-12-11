d1a58b0f65990eb2d73efdbf7244e8d20e925655
Natacha Levesque <natacha.levesque@outlook.com>
2019-12-10 19:16:34 -0500

    version 1.0

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ require('dotenv').config();
 const database = require ('./src/db');
 const express = require('express');
 const bodyParser = require('body-parser');
-const knex = require('knex');
+const knex = require('knex')({
+    client: 'pg',
+    version: '11.5',
+    connection: {
+        host : 'localhost',
+        user : 'natachalevesque',
+        password : '',
+        database : 'space'
+    }
+});
 const app = express();
 const port = 3000;
+
+app.use(express.static('assets'));
 
 app.use(bodyParser.json());
 app.use(
@@ -22,9 +33,22 @@ app.get('/', (request, response) => {
 app.get('/search', async(request, response) => {
 
     const client = await database();
-    const result = await client.query('SELECT * FROM space.star');
+    const table = request.query.celestBody;
+    const name = request.query.name;
+    const result = await client.query(
+
+        knex.select().from( table )
+            .toString()
+
+    );
+
     response.render('search', {results: result.rows});
 
+});
+
+app.get('/result', (request, response) => {
+
+    response.json({ ok: request.query.name})
 });
 
 app.listen(port, () => {
